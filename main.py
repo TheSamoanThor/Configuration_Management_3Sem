41f47ac4aef21e63919199795dbb73b95087df9a
TheSamoanThor <seva_kuzmenko_dec@mail.ru>
2025-09-19 23:06:18 +0300

    Added input from OS console, reading of config file, processing of commands from a file with start script, created logic to avoid bugs from input of args from config and OS console
    Added methods and funcs:
    display_config - to display info about config
    execute_start_script - to execute commands from another file
    parse_arguments - to get args from OS console
    parse_config_file - to read info from config file

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import tkinter as tk
 from tkinter import scrolledtext
 import sys, os # sys for info about the computer, os for curr directory
+import argparse # to get args from the original cmd
+import configparser # to read configs
+
+
 
 class CommandLineInterface:
-    def __init__(self, root):
+    def __init__(self, root, vfs_path, script_path, config_path):
         # Create window + its name
         self.root = root
         self.root.title("CMD ANALOG")
         self.root.geometry("800x600")
         self.root.configure(bg='black')
+        
+        # get paths for files
+        self.vfs_path = vfs_path
+        self.script_path = script_path
+        self.config_path = config_path
         
         # Create the text area for output
         self.output_area = scrolledtext.ScrolledText( #insertbackground - for cursor
@@ -21,12 +30,12 @@ class CommandLineInterface:
         # Create the input field + position it
         self.input_frame = tk.Frame(root, bg='black')
         self.input_frame.pack(fill=tk.X, padx=5, pady=5)
-        
-        # to get curr dir and show it near input field
-        self.prompt = tk.Label( self.input_frame, text=f"{os.getcwd()}>>> ",
+                
+        # to get curr dir and show it near input field, must be updated to show location in VFS
+        self.prompt = tk.Label(self.input_frame, text=f"{os.getcwd()}>>> ",
             bg='black', fg='green', font=('Courier', 12))
         self.prompt.pack(side=tk.LEFT)
-        
+               
         self.input_field = tk.Entry( #command input field
             self.input_frame, bg='black', fg='green', 
             insertbackground='green', font=('Courier', 12), relief=tk.FLAT)
@@ -34,23 +43,82 @@ class CommandLineInterface:
         self.input_field.bind('<Return>', self.execute_command) # if "Enter" is pressed - execute comm
         self.input_field.focus() # Sets focus to the input field so user can type immediately
         
-        # Welcome message
+        # Welcome and about config messages
         self.display_welcome()
-
-        global words
+        self.display_config()
         
+        if self.script_path:
+            self.execute_start_script()
+    
+    
+    
     def display_welcome(self):
         welcome_msg = (
             f"CMD ANALOG On {sys.platform}\n"
             f"Python {sys.version}\n")
         self.display_output(welcome_msg)
     
+    
+    
+    def display_config(self):
+        config_info = (
+            f"Config information:\n"
+            f"Config path: {self.config_path}\n"
+            f"VFS path: {self.vfs_path}\n"
+            f"Script path: {self.script_path}\n"
+            "\n"
+        )
+        self.display_output(config_info)
+    
+       
+
     def display_output(self, text):
         self.output_area.config(state=tk.NORMAL) # Temporarily enables the text widget
         self.output_area.insert(tk.END, text) # Inserts text at the end
         self.output_area.see(tk.END) # Scrolls to the end to show the new text
         self.output_area.config(state=tk.DISABLED) # Disables the text widget again        
         
+
+
+    def execute_start_script(self):
+        with open(self.script_path, 'r') as script_file:
+            commands = script_file.readlines()
+        
+        for command in commands:
+            command = command.strip()
+            if command:
+                # just copied the following code from execute_command(self, event)
+                # Display the command
+                self.display_output(f">>> {command}\n")
+                
+                # split the input line into several words
+                
+                words = [i for i in command.split()]
+                
+                # commands
+                if (command == ''):
+                    pass
+                elif (words[0].lower() in ['exit', 'quit']): # lower - get NON-CAPS version of command
+                    self.display_output("Exiting...\n")
+                    self.root.after(1000, self.root.destroy)
+                elif (words[0].lower() == "help" and len(words)==1):
+                    help_msg = ("help - to see this message\n"
+                                "exit/quit - to stop the program\n"
+                                "ls/cd - not yet implemented\n")
+                    self.display_output(help_msg)
+                elif (words[0].lower() == 'ls'):
+                    self.display_output(f"The command and arguements: {words[0]}, '{words[1:]}'\n")
+                elif (words[0].lower() == 'cd'):
+                    self.display_output(f"The command and arguements: {words[0]}, '{words[1:]}'\n")
+                else:
+                    self.display_output(f"Unknown command: {command}, the following start script cannot be done")
+                    return
+                # stop executing in case of some troubles, as it is in the task
+                    
+                words = []
+                
+
+
     def execute_command(self, event):
         # get command and clear the field
         command = self.input_field.get().strip()
@@ -82,9 +150,65 @@ class CommandLineInterface:
             self.display_output(f"Unknown command: {command}\n")
             
         words = []
-       
+    
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='CMD ANALOG')
+    # description - the first line of the "help" message
+    parser.add_argument('--vfs', type=str,
+                help='Path to VFS physical location, preferable to place this file in the same directory as this code file')
+    parser.add_argument('--script', type=str,
+                help='Path to startup script, preferable to place this file in the same directory as this code file')
+    parser.add_argument('--config', type=str, default='config.ini',
+                help='Path to config file (default: config.ini). Please, place config.ini and the default files in the same directory as this code file')
+    
+    # arguements: (key for search of the arg, type of this arg, the message we get in case of typing "python main.py --help")
+    
+    return parser.parse_args()
+
+
+
+def parse_config_file(config_path):
+    # .ini file - .txt with special structure and saved in a special way
+    # struct:
+    #
+    # [DEFAULT]
+    # vfs_path = vfs_struct_file.txt
+    # script_path = vfs_start_script.txt
+    #
+        
+    config = configparser.ConfigParser()
+    vfs_path = None
+    script_path = None
+    
+    if os.path.exists(config_path):
+        config.read(config_path)
+        if 'DEFAULT' in config:
+            if 'vfs_path' in config['DEFAULT']:
+                vfs_path = config['DEFAULT']['vfs_path']
+            if 'script_path' in config['DEFAULT']:
+                script_path = config['DEFAULT']['script_path']
+    
+    return vfs_path, script_path
+
+
 
 if __name__ == "__main__":
+    args = parse_arguments()
+    
+    vfs_path, script_path = parse_config_file(args.config)
+    
+    if args.vfs:
+        vfs_path = args.vfs
+    if args.script:
+        script_path = args.script
+    
+    # if file is open from cmd, then it prints information in concole
+    print(f"Info: VFS path = {vfs_path}")
+    print(f"Info: Script path = {script_path}")
+    print(f"Info: Config file = {args.config}")
+    
     root = tk.Tk()
-    cli = CommandLineInterface(root)
+    cli = CommandLineInterface(root, vfs_path, script_path, args.config)
     root.mainloop()
