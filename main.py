6728c768d8e758d2d887e330ec65119903bb6796
TheSamoanThor <seva_kuzmenko_dec@mail.ru>
2025-09-22 02:17:33 +0300

    Update main.py
    added VFS, which needed VFS and VFSNode classes and their methods. Minor changes in CommandLineInterface __init__ method for creating the vfs

--- main.py
+++ main.py
@@ -3,8 +3,111 @@ from tkinter import scrolledtext
 import sys, os # sys for info about the computer, os for curr directory
 import argparse # to get args from the original cmd
 import configparser # to read configs
+import json
+import base64
 
 
+
+class VFSNode:
+#     __slots__ = ('name', 'type', 'content', 'children', 'parent', 'metadata', 'path')       
+    def __init__(self, node_data):
+        
+        self.name = node_data['name']
+        self.type = node_data['type']
+        self.path = node_data['path']
+        self.parent = None
+        self.children = {}
+        
+        if ('encoding' in node_data):
+            self.encoding = node_data['encoding']
+        else:
+            self.encoding = None
+        
+        if ('content' in node_data):
+            if self.encoding == 'base64': # decode if encoded
+                self.content = base64.b64decode(node_data['content']).decode('utf-8')
+            else:
+                self.content = node_data['content'].decode('utf-8')
+        else:
+            self.content = None
+            
+        if ('metadata' in node_data):
+            self.metadata = node_data['metadata']
+        else:
+            self.metadata = None
+            
+        
+        
+    def add_child(self, node):
+        node.parent = self
+        #add to dict
+        self.children[node.name] = node
+        
+        
+        
+    def get_path(self):
+        if (not(self.path)):
+            path = []
+            current = self
+            while current:
+                path.append(current.name)
+                current = current.parent
+            return '/'.join(reversed(path))
+        return self.path
+    
+    
+    
+    def display_data(self):
+        print(f"Data: \n"+
+              f"name = {self.name}\n"+
+              f"type = {self.type}\n"+
+              f"path = {self.path}\n"+
+              f"parent = {self.parent}\n"+
+              f"children = {self.children}\n"+
+              f"content = {self.content}\n"+
+              f"metadata = {self.metadata}\n")
+       
+       
+       
+class VFS:
+    def __init__(self, vfs_path):
+        self.root = self.load_vfs_from_json(vfs_path)
+               
+        
+        
+    def load_vfs_from_json(self, json_path):
+        with open(json_path, 'r', encoding='utf-8') as f:
+            vfs_data = json.load(f)
+                
+        # Создаем корневой узел VFS
+        return self.build_vfs_tree(vfs_data)
+    
+
+
+    def build_vfs_tree(self, node_data, parent=None):
+               
+        if node_data['type'] == 'directory':
+            # Создаем узел директории
+            node = VFSNode(node_data)
+            if parent:
+                parent.add_child(node)
+            
+            # Рекурсивно обрабатываем дочерние элементы
+            if 'children' in node_data:
+                for child_data in node_data['children']:
+                    self.build_vfs_tree(child_data, node)
+            node.display_data()
+            return node
+            
+        elif node_data['type'] == 'file':
+            # Создаем узел файла
+            node = VFSNode(node_data)
+            if parent:
+                parent.add_child(node)
+            node.display_data()
+            return node
+    
+    
 
 class CommandLineInterface:
     def __init__(self, root, vfs_path, script_path, config_path):
@@ -21,8 +124,8 @@ class CommandLineInterface:
         
         # Create the text area for output
         self.output_area = scrolledtext.ScrolledText( #insertbackground - for cursor
-            root, wrap=tk.WORD, bg='black', fg='green', 
-            insertbackground='green', font=('Courier', 12))
+            root, wrap=tk.WORD, bg='black', fg='white', 
+            insertbackground='white', font=('Courier', 12))
         # The pack() method positions it in the window with expansion and padding
         self.output_area.pack(expand=True, fill='both', padx=5, pady=5)
         self.output_area.config(state=tk.DISABLED)
@@ -33,15 +136,24 @@ class CommandLineInterface:
                 
         # to get curr dir and show it near input field, must be updated to show location in VFS
         self.prompt = tk.Label(self.input_frame, text=f"{os.getcwd()}>>> ",
-            bg='black', fg='green', font=('Courier', 12))
+            bg='black', fg='white', font=('Courier', 12))
         self.prompt.pack(side=tk.LEFT)
                
         self.input_field = tk.Entry( #command input field
-            self.input_frame, bg='black', fg='green', 
-            insertbackground='green', font=('Courier', 12), relief=tk.FLAT)
+            self.input_frame, bg='black', fg='white', 
+            insertbackground='white', font=('Courier', 12), relief=tk.FLAT)
         self.input_field.pack(side=tk.LEFT, fill=tk.X, expand=True) # expand horizontally
         self.input_field.bind('<Return>', self.execute_command) # if "Enter" is pressed - execute comm
         self.input_field.focus() # Sets focus to the input field so user can type immediately
+        
+        #get vfs
+        self.vfs = None
+        if vfs_path and os.path.exists(vfs_path):
+            self.vfs = VFS(vfs_path)
+        else:
+            self.vfs = None
+            self.display_output("VFS не загружена: файл не найден или путь не указан\n")
+        self.vfs_exists = (self.vfs!=None)  
         
         # Welcome and about config messages
         self.display_welcome()
@@ -65,6 +177,7 @@ class CommandLineInterface:
             f"Config information:\n"
             f"Config path: {self.config_path}\n"
             f"VFS path: {self.vfs_path}\n"
+            f"VFS exists: {self.vfs_exists}\n"
             f"Script path: {self.script_path}\n"
             "\n"
         )
@@ -153,7 +266,7 @@ class CommandLineInterface:
     
 
 
-def parse_arguments():
+def parse_arguments(): 
     parser = argparse.ArgumentParser(description='CMD ANALOG')
     # description - the first line of the "help" message
     parser.add_argument('--vfs', type=str,
@@ -191,7 +304,7 @@ def parse_config_file(config_path):
                 script_path = config['DEFAULT']['script_path']
     
     return vfs_path, script_path
-
+     
 
 
 if __name__ == "__main__":
@@ -199,6 +312,7 @@ if __name__ == "__main__":
     
     vfs_path, script_path = parse_config_file(args.config)
     
+    # priority
     if args.vfs:
         vfs_path = args.vfs
     if args.script:
